26d359fb9b2c518dd762b0acc95edcb5453f0390
annahiro <anna@hiro-media.com>
2018-11-13 16:51:55 +0200

    Create plot_roc_curve.py

---
+++ infra_functions/plot_roc_curve.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+
+def plot_roc_curve(fpr,tpr):
+    roc_auc = metrics.auc(fpr, tpr)
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
