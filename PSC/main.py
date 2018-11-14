f0e2eb937931243616382a3a9c45c598a3ca3962
annahiro <anna@hiro-media.com>
2018-11-14 17:24:42 +0200

    Update main.py

--- PSC/main.py
+++ PSC/main.py
@@ -2,10 +2,11 @@ from load_merge_otu_mf import OtuMfHandler
 from preprocess import preprocess_data
 from pca import *
 from plot_confusion_matrix import *
+from plot_roc_curve import *
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold,LeaveOneOut
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from xgboost import XGBClassifier
@@ -17,9 +18,9 @@ preproccessed_data = preprocess_data(OtuMf.otu_file, visualize_data=False, taxno
 #visualize_pca(preproccessed_data)
 
 otu_after_pca = apply_pca(preproccessed_data, n_components=30)
-merged_data = otu_after_pca.join(OtuMf.mapping_file['DiagnosisGroup'])
-
+merged_data = otu_after_pca.join(OtuMf.mapping_file['Diagnosis'])
 merged_data.fillna(0)
+
 
 # font = {'size': 22}
 # max_pca = 5
@@ -45,15 +46,26 @@ merged_data.fillna(0)
 #                 k+=1
 # plt.legend( loc=1,ncol=1)
 # plt.show()
-mapping_disease_for_labels = {'Control':0,'Cirrhosis/HCC':1, 'PSC/PSC+IBD':2}
-mapping_disease = {'Control':0,'Cirrhosis ':1, 'HCC':1, 'PSC+IBD':2,'PSC':2}
-merged_data['DiagnosisGroup'] = merged_data['DiagnosisGroup'].map(mapping_disease)
+mapping_disease_for_labels = {'Control':0,'Cirrhosis/+HCC':1, 'HCC':2, 'PSC/+IBD':3}
+mapping_disease = {'Control':0,'Cirrhosis ':1,'CirrhosisHCC':1, 'HCC':1 , 'PSC+IBD':2,'PSC':2}
+merged_data['Diagnosis'] = merged_data['Diagnosis'].map(mapping_disease)
 merged_data = merged_data.join(OtuMf.mapping_file[['Age', 'BMI', 'FattyLiver','RegularExercise', 'Smoking']])
 mappin_boolean = {'yes' :1, 'no': 0, 'Control': 0, '0':0, '1':1}
 merged_data['FattyLiver'] = merged_data['FattyLiver'].map(mappin_boolean)
 merged_data['RegularExercise'] = merged_data['RegularExercise'].map(mappin_boolean)
 merged_data['Smoking'] = merged_data['Smoking'].map(mappin_boolean)
-print(merged_data.tail())
+#merged_data = merged_data[np.isfinite(merged_data['DiagnosisGroup'])]
+
+#plot ROC curve
+# X_train, X_test, y_train, y_test = train_test_split(
+#                            merged_data.loc[:, merged_data.columns != 'DiagnosisGroup'], merged_data['DiagnosisGroup'],
+#                            test_size=0.25)
+# model = XGBClassifier(max_depth=4,n_estimators = 100 ,learning_rate = 10/100,
+#                                            objective= 'binary:logistic')
+# model.fit(X_train, y_train)
+# y_pred = model.predict_proba(X_test)[:,1]
+# fpr, tpr, threshold = metrics.roc_curve(y_test, y_pred)
+# plot_roc_curve(fpr,tpr)
 
 #Random Forest
 # for j in range(6, 20, 2):
@@ -85,22 +97,34 @@ print(merged_data.tail())
 #                 #print(k, j, l,sum(accuracy)/len(accuracy) )
 
 
+X = merged_data.loc[:, merged_data.columns != 'Diagnosis']
+y = merged_data['Diagnosis']
 
-# XGBoost fit model no training data
-# for md in range(3,7):
-#      for ne in range (100,400,50):
-#           for lr in range (5, 25, 5):
-#               accuracy = []
+loo = LeaveOneOut()
+#skf = RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=42)
+# XGBoost fit model no training
+
+
+for md in range(3,4):
+      for ne in range (100,150,50):
+           for lr in range (10, 15, 5):
+               accuracy = []
 # # #             auc_train = []
 # # #             auc = []
-#               for i in range(0,20):
-#                       X_train, X_test, y_train, y_test = train_test_split(
-#                           merged_data.loc[:, merged_data.columns != 'DiagnosisGroup'], merged_data['DiagnosisGroup'],
-#                           test_size=0.2)
-#                       model = XGBClassifier(max_depth=md,n_estimators = ne ,learning_rate = lr/100,  objective='multi:softmax' ,random_state=i )
+               #for i in range(0,40,2):
+               #        X_train, X_test, y_train, y_test = train_test_split(
+               #            merged_data.loc[:, merged_data.columns != 'DiagnosisGroup'], merged_data['DiagnosisGroup'],
+               #            test_size=0.25, random_state=i)
+               y_pred_list = []
+               for train_index, test_index in loo.split(X):
+                    train_index=list(train_index)
+                    #print("%s %s" % (train_index, test_index))
+                    X_train, X_test = X.iloc[train_index,:], X.iloc[test_index,:]
+                    y_train, y_test = y[train_index], y[test_index]
+                    model = XGBClassifier(max_depth=md,n_estimators = ne ,learning_rate = lr/100,  objective='multi:softmax' )
 # # #                                           #objective= 'binary:logistic')
-#                       model.fit(X_train, y_train)
-#                       y_pred = model.predict(X_test)
+                    model.fit(X_train, y_train)
+                    y_pred = model.predict(X_test)
 # # #                     #pred_train = model.predict_proba(X_train)[:, 1]
 # # #                     #auc_train.append(metrics.roc_auc_score(y_train, pred_train))
 # # #                     #y_pred = model.predict_proba(X_test)[:,1]
@@ -108,31 +132,48 @@ print(merged_data.tail())
 # # #                     #    auc.append(metrics.roc_auc_score(y_test, y_pred))
 # # #                     #except:
 # # #                     #    continue
-#                       accuracy.append(metrics.accuracy_score(y_test,y_pred))
-#               print(md, ne, lr,  round(sum(accuracy)/len(accuracy),2), accuracy)
-# #
+                    y_pred_list.append(y_pred)
+                    #accuracy.append(metrics.accuracy_score(y_test,y_pred))
+               cnf_matrix = metrics.confusion_matrix(y,y_pred_list)
+               class_names = mapping_disease_for_labels.keys()
+               # # Plot non-normalized confusion matrix
+               plt.figure()
+               plot_confusion_matrix(cnf_matrix, classes=class_names,
+                                        title='Confusion matrix, without normalization')
+
+               # # Plot normalized confusion matrix
+               plt.figure()
+               plot_confusion_matrix(cnf_matrix, classes=list(class_names), normalize=True,
+                                        title='Normalized confusion matrix')
+
+               plt.show()
+               scores = np.array(metrics.accuracy_score(y,y_pred_list))
+               print(md, ne, lr,  round(scores.mean(),2), round(scores.std(),2) * 2)
+#plot roc_curve #
+
+
 
 
 #
-X_train, X_test, y_train, y_test = train_test_split(
-                          merged_data.loc[:, merged_data.columns != 'DiagnosisGroup'], merged_data['DiagnosisGroup'],
-                          test_size=0.2)
-model = XGBClassifier(max_depth=4,n_estimators = 200 ,learning_rate = 10/100,  objective='multi:softmax' ,random_state=4 )
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
-accuracy = metrics.accuracy_score(y_test,y_pred)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
-print(cnf_matrix)
-class_names = mapping_disease_for_labels.keys()
-# # # Plot non-normalized confusion matrix
-plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=class_names,
-                        title='Confusion matrix, without normalization')
-
-# # # Plot normalized confusion matrix
-plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=list(class_names), normalize=True,
-                        title='Normalized confusion matrix')
-
-plt.show()
+# X_train, X_test, y_train, y_test = train_test_split(
+#                           merged_data.loc[:, merged_data.columns != 'DiagnosisGroup'], merged_data['DiagnosisGroup'],
+#                           test_size=0.2)
+# model = XGBClassifier(max_depth=3,n_estimators = 300 ,learning_rate = 15/100,  objective='multi:softmax' ,random_state=2 )
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+# cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+# accuracy = metrics.accuracy_score(y_test,y_pred)
+# print("Accuracy: %.2f%%" % (accuracy * 100.0))
+# print(cnf_matrix)
+# class_names = mapping_disease_for_labels.keys()
+# # # # Plot non-normalized confusion matrix
+# plt.figure()
+# plot_confusion_matrix(cnf_matrix, classes=class_names,
+#                         title='Confusion matrix, without normalization')
+#
+# # # # Plot normalized confusion matrix
+# plt.figure()
+# plot_confusion_matrix(cnf_matrix, classes=list(class_names), normalize=True,
+#                         title='Normalized confusion matrix')
+#
+# plt.show()
