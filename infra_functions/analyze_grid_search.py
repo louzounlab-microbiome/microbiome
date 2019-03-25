1306a535061b0141862c924088d4d072e30d82d5
bardubovski <bar.dubovski@gmail.com>
2019-03-25 16:54:26 +0200

    changed the anaylsis way of gridsearch

--- infra_functions/analyze_grid_search.py
+++ infra_functions/analyze_grid_search.py
@@ -18,8 +18,20 @@ grid_search_file = r'C:\Thesis\GridSearch_combine_algo_new_loss_50_times\grid_se
 grid_search_file = r'C:\Thesis\allergy\GridSearch_epochs_factors_dropout_l2_layers_neurons\grid_search_results.txt'
 grid_search_file = r'C:\Thesis\allergy\grid_search_no_censored\grid_search_results.txt'
 
-grid_search_folder = r'C:\Thesis\allergy_multi_grid_xgboost_with_similiarity__'
+grid_search_folder = r'C:\Thesis\gvhd_multi_grid_tf_without_censored'
 grid_search_file = os.path.join(grid_search_folder, 'grid_search_results.txt')
+
+
+grid_search_folder = r'C:\Thesis\multi_process_analysis\allergy_multi_grid_xgboost_with_similiarity'
+grid_search_folder = r'C:\Thesis\multi_process_analysis\allergy_multi_grid_xgboost_without_similiarity'
+grid_search_folder = r'C:\Thesis\multi_process_analysis\gvhd_multi_grid_xgboost_with_similiarity'
+grid_search_folder = r'C:\Thesis\multi_process_analysis\gvhd_multi_grid_xgboost_without_similiarity'
+
+
+grid_search_folder = r'C:\Thesis\gvhd_multi_grid_xgboost_with_similiarity'
+# grid_search_folder = r'C:\Thesis\allergy_multi_grid_xgboost_with_similiarity'
+# grid_search_folder = r'C:\Thesis\allergy_multi_grid_xgboost_without_similiarity'
+# grid_search_folder = r'C:\Thesis\gvhd_multi_grid_xgboost_without_similiarity'
 
 def print_recursive(object_to_print, count=0):
     spaces = count * ' '
@@ -42,14 +54,41 @@ def print_recursive(object_to_print, count=0):
         # spaces = (count-1) * '\t'+ ' '
         print(f'{spaces}{object_to_print}')
 
+
+def print_recursive2(object_to_print, count=0, str_to_return=''):
+    spaces = count * ' '
+    if type(object_to_print) is dict:
+        count += 1
+        for key, val in object_to_print.items():
+            if key in ['raw_stats', 'hist']:
+                continue
+            if key in ['rho', 'pval', 'mse']:
+                str_to_return += f'{spaces}{key}: {val}'
+            else:
+                if 'best_' in key:
+                    str_to_return += f'\n{spaces}{key}'
+                else:
+                    str_to_return += f'{spaces}{key}'
+
+                try:
+                    str_to_return += print_recursive2(val, count=count, str_to_return=str_to_return)
+                except:
+                    pass
+    else:
+        # spaces = (count-1) * '\t'+ ' '
+        str_to_return += f'{spaces}{object_to_print}'
+    return str_to_return
+
 def get_stats_for_array(array):
-    return {'mean': array.mean(), 'std': array.std(), 'max': array.max(), 'min': array.min(), 'median': np.median(array)}
+    return {'mean': array.mean(), 'std': array.std(), 'max': array.max(), 'min': array.min(),
+            'median': np.median(array)}
+
 
 def calc_stats_for_config():
     different_configs = [os.path.join(grid_search_folder, o) for o in os.listdir(grid_search_folder)
                          if os.path.isdir(os.path.join(grid_search_folder, o))]
 
-    configuration_stats ={}
+    configuration_stats = {}
     print(f'Total different configs: {len(different_configs)}')
     for config in different_configs:
         grid_search_file = os.path.join(config, 'grid_search_results.txt')
@@ -66,19 +105,18 @@ def calc_stats_for_config():
         total_train_mse = []
         total_train_rho = []
 
-        count=0
-        for idx in range(0, int(len(grid_search_data)/6)):
-            configuration_set = grid_search_data[idx*6:(idx+1)*6]
+        count = 0
+        for idx in range(0, int(len(grid_search_data) / 4)):
+            configuration_set = grid_search_data[idx * 4:(idx + 1) * 4]
             try:
-                current_config = {configuration_set[2][:-1]: eval(configuration_set[3][1:-1])}
-                current_config.update({configuration_set[4][:-1]: eval(configuration_set[5][1:-1])})
+                current_config = {configuration_set[0][:-1]: eval(configuration_set[1][1:-1])}
+                current_config.update({configuration_set[2][:-1]: eval(configuration_set[3][1:-1])})
                 test_mse = current_config['Test']['mse']
                 train_mse = current_config['Train']['mse']
                 test_rho = current_config['Test']['spearman']
                 train_rho = current_config['Train']['spearman']
                 test_pearson_rho = current_config['Test']['pearson']
                 train_pearson_rho = current_config['Train']['pearson']
-
 
                 total_test_rho.append(test_rho)
                 total_test_mse.append(test_mse)
@@ -88,28 +126,39 @@ def calc_stats_for_config():
                 total_train_mse.append(train_mse)
                 total_train_pearson_rho.append(train_pearson_rho)
 
-                count+=1
+                count += 1
             except:
                 pass
-            idx+=6
+            idx += 4
 
         # get_stats_for_array([x['rho'] for x in total_train_rho])
-        configuration_stats[config_name] = {'raw_stats': {'total_train_rho': total_train_rho, 'total_train_mse': total_train_mse,
-                                                          'total_test_rho': total_test_rho, 'total_test_mse': total_test_mse},
-                                            'train_rho': {'rho': get_stats_for_array(np.array([x['rho'] for x in total_train_rho])), 'pval': get_stats_for_array(np.array([x['pvalue'] for x in total_train_rho]))},
-                                            'train_mse': {'mse': get_stats_for_array(np.array(total_train_mse))},
-                                            'train_pearson_rho': {'rho': get_stats_for_array(np.array([x['rho'] for x in total_train_pearson_rho])), 'pval': get_stats_for_array(np.array([x['pvalue'] for x in total_train_pearson_rho]))},
-                                            'test_rho': {'rho': get_stats_for_array(np.array([x['rho'] for x in total_test_rho])), 'pval': get_stats_for_array(np.array([x['pvalue'] for x in total_test_rho]))},
-                                            'test_mse': {'mse': get_stats_for_array(np.array(total_test_mse))},
-                                            'test_pearson_rho': {'rho': get_stats_for_array(np.array([x['rho'] for x in total_test_pearson_rho])), 'pval': get_stats_for_array(np.array([x['pvalue'] for x in total_test_pearson_rho]))},
-                                            'hist': hist_file,
-                                            'Total number of records in config': len(total_test_rho)
-                                            }
+        configuration_stats[config_name] = {
+            'raw_stats': {'total_train_rho': total_train_rho, 'total_train_mse': total_train_mse,
+                          'total_test_rho': total_test_rho, 'total_test_mse': total_test_mse},
+            'test_rho': {'rho': get_stats_for_array(
+                np.array([x['rho'] for x in total_test_rho])),
+                'pval': get_stats_for_array(
+                    np.array([x['pvalue'] for x in total_test_rho]))},
+            'test_mse': {'mse': get_stats_for_array(np.array(total_test_mse))},
+            'test_pearson_rho': {'rho': get_stats_for_array(
+                np.array([x['rho'] for x in total_test_pearson_rho])),
+                'pval': get_stats_for_array(np.array(
+                    [x['pvalue'] for x in total_test_pearson_rho]))},
+
+            'train_rho': {'rho': get_stats_for_array(np.array([x['rho'] for x in total_train_rho])),
+                          'pval': get_stats_for_array(np.array([x['pvalue'] for x in total_train_rho]))},
+            'train_mse': {'mse': get_stats_for_array(np.array(total_train_mse))},
+            'train_pearson_rho': {'rho': get_stats_for_array(np.array([x['rho'] for x in total_train_pearson_rho])),
+                                  'pval': get_stats_for_array(
+                                      np.array([x['pvalue'] for x in total_train_pearson_rho]))},
+            'hist': hist_file,
+            'Total number of records in config': len(total_test_rho)
+            }
 
     return configuration_stats
 
-def find_best_config(configuration_stats):
 
+def find_best_config(configuration_stats):
     best_test_rho = 0
     best_combined_mse = 99999999999
     best_test_mse = 99999999999
@@ -123,15 +172,15 @@ def find_best_config(configuration_stats):
 
         if test_rho > best_test_rho:
             best_test_rho = test_rho
-            best_test_rho_config=key
+            best_test_rho_config = key
 
-        if test_mse  < best_test_mse and train_rho > 0 and test_rho > 0:
+        if test_mse < best_test_mse and train_rho > 0 and test_rho > 0:
             best_test_mse = test_mse
-            best_test_mse_config=key
+            best_test_mse_config = key
 
         if test_mse + train_mse < best_combined_mse and train_rho > 0 and test_rho > 0:
             best_combined_mse = test_mse + train_mse
-            best_combined_mse_config=key
+            best_combined_mse_config = key
 
     def get_stats_for_best_config(config):
         test_mse = configuration_stats[config]['test_mse']
@@ -140,14 +189,17 @@ def find_best_config(configuration_stats):
         train_rho = configuration_stats[config]['rain_rho']
         return {'train_rho': train_rho, 'train_mse': train_mse, 'test_rho': test_rho, 'test_mse': test_mse}
 
-    best_configs = {'best_test_rho_config': {'name': best_test_rho_config, 'stats': configuration_stats[best_test_rho_config]},
-                    'best_test_mse_config': {'name': best_test_mse_config, 'stats': configuration_stats[best_test_mse_config]},
-                    'best_combined_mse_config': {'name': best_combined_mse_config, 'stats': configuration_stats[best_combined_mse_config]}}
+    best_configs = {
+        'best_test_rho_config': {'name': best_test_rho_config, 'stats': configuration_stats[best_test_rho_config]},
+        'best_test_mse_config': {'name': best_test_mse_config, 'stats': configuration_stats[best_test_mse_config]},
+        'best_combined_mse_config': {'name': best_combined_mse_config,
+                                     'stats': configuration_stats[best_combined_mse_config]}}
 
     return best_configs
 
+
 def main():
-    print(f'Analyzing {grid_search_file}')
+    print(f'Analyzing {grid_search_folder}')
     configuration_stats = calc_stats_for_config()
     best_configs = find_best_config(configuration_stats)
 
@@ -156,31 +208,29 @@ def main():
     with open(best_configs['best_test_rho_config']['stats']['hist'], 'rb') as f:
         a = pickle.load(f)
 
-    print_recursive(best_configs)
+    a=print_recursive(best_configs)
     pass
 
+
 def old_analyze():
-
-
-    with open(grid_search_file,'r') as f:
+    with open(grid_search_file, 'r') as f:
         grid_search_data = f.readlines()
 
-
-    configuration_stats ={}
-    best_mse=10000
-    best_config=''
-    best_mse_config=''
-    best_rho=-100
+    configuration_stats = {}
+    best_mse = 10000
+    best_config = ''
+    best_mse_config = ''
+    best_rho = -100
     hist_mse_best = None
     hist_rho_best = None
     best_rho_config = ''
-    best_mse_config= ''
-    total_test_rho=[]
-    total_test_mse=[]
-    count=0
-    for idx in range(0, int(len(grid_search_data)/6)):
-        configuration_set = grid_search_data[idx*6:(idx+1)*6]
-        current_config=''
+    best_mse_config = ''
+    total_test_rho = []
+    total_test_mse = []
+    count = 0
+    for idx in range(0, int(len(grid_search_data) / 6)):
+        configuration_set = grid_search_data[idx * 6:(idx + 1) * 6]
+        current_config = ''
         try:
             current_config = {configuration_set[2][:-1]: eval(configuration_set[3][1:-1])}
             current_config.update({configuration_set[4][:-1]: eval(configuration_set[5][1:-1])})
@@ -190,21 +240,23 @@ def old_analyze():
             train_rho = current_config['Train']['spearman']['rho']
 
             if test_rho > best_rho:
-                best_rho=test_rho
+                best_rho = test_rho
                 best_rho_config = {configuration_set[1][:-1]: current_config}
-                hist_rho_best = os.path.join(os.path.split(grid_search_file)[0], configuration_set[1][:-1], 'hist_res.p')
-            if test_mse+train_mse < best_mse and train_rho>0 and test_rho>0:
-                best_mse = test_mse+train_mse
+                hist_rho_best = os.path.join(os.path.split(grid_search_file)[0], configuration_set[1][:-1],
+                                             'hist_res.p')
+            if test_mse + train_mse < best_mse and train_rho > 0 and test_rho > 0:
+                best_mse = test_mse + train_mse
                 best_mse_config = {configuration_set[1][:-1]: current_config}
-                hist_mse_best = os.path.join(os.path.split(grid_search_file)[0],configuration_set[1][:-1], 'hist_res.p')
+                hist_mse_best = os.path.join(os.path.split(grid_search_file)[0], configuration_set[1][:-1],
+                                             'hist_res.p')
 
             total_test_rho.append(test_rho)
             total_test_mse.append(test_mse)
-            count+=1
+            count += 1
         except:
             pass
-        configuration_stats[configuration_set[1][:-1]]=current_config
-        idx+=6
+        configuration_stats[configuration_set[1][:-1]] = current_config
+        idx += 6
 
     total_test_rho = np.array(total_test_rho)
     total_test_mse = np.array(total_test_mse)
@@ -216,25 +268,25 @@ def old_analyze():
     print_recursive(best_rho_config)
 
     if use_hist:
-
         with open(hist_rho_best, 'rb') as f:
-            a=pickle.load(f)
+            a = pickle.load(f)
         plt.figure(str + '\n' + list(best_rho_config.keys())[0])
         plt.plot(a['loss'])
         plt.title('Train loss vs epoch of the best Spearman rho on test\n' + list(best_rho_config.keys())[0])
 
     str = 'Best Test+Train MSE'
-    print('\n'+str)
+    print('\n' + str)
     print_recursive(best_mse_config)
 
     if use_hist:
         with open(hist_mse_best, 'rb') as f:
-            a=pickle.load(f)
+            a = pickle.load(f)
         plt.figure(str + '\n' + list(best_rho_config.keys())[0])
         plt.plot(a['loss'])
         plt.title('Train loss vs epoch of the best MSE on test\n' + list(best_mse_config.keys())[0])
         #
         # plt.show()
+
 
 if __name__ == '__main__':
     main()
