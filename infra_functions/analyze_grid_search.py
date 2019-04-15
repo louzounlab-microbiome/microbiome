f8e9b86626c9f6a5a35889a94ca3ec263849f429
bardubovski <bar.dubovski@gmail.com>
2019-04-15 16:15:37 +0300

    analyze of grid search

--- infra_functions/analyze_grid_search.py
+++ infra_functions/analyze_grid_search.py
@@ -5,30 +5,30 @@ import numpy as np
 
 use_hist = False
 
-# grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_my_loss_corrected\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_mse_loss\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_mse_loss\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_my_loss_factor_layers_neurons\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_my_loss_fine\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\subject_test_100_processes_true\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\subject_test_100_processes_mse_false\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\subject_test_100_processes_myloss_USE_SUBJECT_ONLY_TEST_TRAIN=True\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_combine_algo_new_loss_epochs_dropout_l2_numberlayers_numberneurons\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\GridSearch_combine_algo_new_loss_50_times\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\allergy\GridSearch_epochs_factors_dropout_l2_layers_neurons\grid_search_results.txt'
-grid_search_file = r'C:\Thesis\allergy\grid_search_no_censored\grid_search_results.txt'
-
-grid_search_folder = r'C:\Thesis\gvhd_multi_grid_tf_without_censored'
-grid_search_file = os.path.join(grid_search_folder, 'grid_search_results.txt')
-
-
-grid_search_folder = r'C:\Thesis\multi_process_analysis\allergy_multi_grid_xgboost_with_similiarity'
-grid_search_folder = r'C:\Thesis\multi_process_analysis\allergy_multi_grid_xgboost_without_similiarity'
-grid_search_folder = r'C:\Thesis\multi_process_analysis\gvhd_multi_grid_xgboost_with_similiarity'
+# # grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_my_loss_corrected\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_mse_loss\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_mse_loss\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_my_loss_factor_layers_neurons\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_my_loss_fine\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\subject_test_100_processes_true\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\subject_test_100_processes_mse_false\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\subject_test_100_processes_myloss_USE_SUBJECT_ONLY_TEST_TRAIN=True\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\GVHD_BAR\GridSearch_combine_algo_new_loss_epochs_dropout_l2_numberlayers_numberneurons\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\GridSearch_combine_algo_new_loss_50_times\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\allergy\GridSearch_epochs_factors_dropout_l2_layers_neurons\grid_search_results.txt'
+# grid_search_file = r'C:\Thesis\allergy\grid_search_no_censored\grid_search_results.txt'
+#
+# grid_search_folder = r'C:\Thesis\gvhd_multi_grid_tf_without_censored'
+# grid_search_file = os.path.join(grid_search_folder, 'grid_search_results.txt')
+#
+#
+# grid_search_folder = r'C:\Thesis\multi_process_analysis\allergy_multi_grid_xgboost_with_similiarity'
+# grid_search_folder = r'C:\Thesis\multi_process_analysis\allergy_multi_grid_xgboost_without_similiarity'
+# grid_search_folder = r'C:\Thesis\multi_process_analysis\gvhd_multi_grid_xgboost_with_similiarity'
 grid_search_folder = r'C:\Thesis\multi_process_analysis\gvhd_multi_grid_xgboost_without_similiarity'
 
 
-grid_search_folder = r'C:\Thesis\gvhd_multi_grid_xgboost_with_similiarity'
+# grid_search_folder = r'C:\Thesis\gvhd_multi_grid_xgboost_with_similiarity'
 # grid_search_folder = r'C:\Thesis\allergy_multi_grid_xgboost_with_similiarity'
 # grid_search_folder = r'C:\Thesis\allergy_multi_grid_xgboost_without_similiarity'
 # grid_search_folder = r'C:\Thesis\gvhd_multi_grid_xgboost_without_similiarity'
@@ -55,36 +55,160 @@ def print_recursive(object_to_print, count=0):
         print(f'{spaces}{object_to_print}')
 
 
-def print_recursive2(object_to_print, count=0, str_to_return=''):
+def print_recursive2(object_to_print, count=0):
     spaces = count * ' '
+    str_to_return =''
     if type(object_to_print) is dict:
         count += 1
         for key, val in object_to_print.items():
             if key in ['raw_stats', 'hist']:
                 continue
             if key in ['rho', 'pval', 'mse']:
-                str_to_return += f'{spaces}{key}: {val}'
+                str_to_return += f'{spaces}{key}: {val}\n'
             else:
                 if 'best_' in key:
-                    str_to_return += f'\n{spaces}{key}'
+                    str_to_return += f'\n{spaces}{key}\n'
                 else:
-                    str_to_return += f'{spaces}{key}'
+                    str_to_return += f'{spaces}{key}\n'
 
                 try:
-                    str_to_return += print_recursive2(val, count=count, str_to_return=str_to_return)
+                    str_to_return += print_recursive2(val, count=count)
                 except:
                     pass
     else:
-        # spaces = (count-1) * '\t'+ ' '
-        str_to_return += f'{spaces}{object_to_print}'
+        str_to_return += f'{spaces}{object_to_print}\n'
     return str_to_return
 
 def get_stats_for_array(array):
     return {'mean': array.mean(), 'std': array.std(), 'max': array.max(), 'min': array.min(),
             'median': np.median(array)}
 
+def filter_configs(grid_search_folder, filter_dict=None):
+    different_configs = [os.path.join(grid_search_folder, o) for o in os.listdir(grid_search_folder)
+                         if os.path.isdir(os.path.join(grid_search_folder, o))]
+    if filter_dict is None:
+        return different_configs
 
-def calc_stats_for_config():
+    filtered_configs = []
+    for config in different_configs:
+        config_name = os.path.basename(config)
+        parameters = config_name.split('^')
+        add_flag = True
+        for key, values in filter_dict.items():
+            cur_filter_flag = False
+            for val in values:
+                cur_filter = f'{key}={val}'
+                if cur_filter in parameters:
+                    cur_filter_flag = True
+            if not cur_filter_flag:
+                add_flag = False
+
+        if add_flag:
+            filtered_configs.append(config)
+    return filtered_configs
+
+
+def calc_result_per_param(different_configs):
+
+    configuration_results = {}
+    print(f'Total different configs: {len(different_configs)}')
+    for config in different_configs:
+        grid_search_file = os.path.join(config, 'grid_search_results.txt')
+        config_name = os.path.basename(config)
+        parameters = config_name.split('^')
+
+        with open(grid_search_file, 'r') as f:
+            grid_search_data = f.readlines()
+
+        total_test_pearson_rho = []
+        total_test_mse = []
+        total_test_rho = []
+
+        total_train_pearson_rho = []
+        total_train_mse = []
+        total_train_rho = []
+
+        count = 0
+        for idx in range(0, int(len(grid_search_data) / 4)):
+            configuration_set = grid_search_data[idx * 4:(idx + 1) * 4]
+            try:
+                current_config = {configuration_set[0][:-1]: eval(configuration_set[1][1:-1])}
+                current_config.update({configuration_set[2][:-1]: eval(configuration_set[3][1:-1])})
+                test_mse = current_config['Test']['mse']
+                train_mse = current_config['Train']['mse']
+                test_rho = current_config['Test']['spearman']
+                train_rho = current_config['Train']['spearman']
+                test_pearson_rho = current_config['Test']['pearson']
+                train_pearson_rho = current_config['Train']['pearson']
+
+                total_test_rho.append(test_rho)
+                total_test_mse.append(test_mse)
+                total_test_pearson_rho.append(test_pearson_rho)
+
+                total_train_rho.append(train_rho)
+                total_train_mse.append(train_mse)
+                total_train_pearson_rho.append(train_pearson_rho)
+
+                count += 1
+            except:
+                pass
+            idx += 4
+
+        for param in parameters:
+            param_name_val = param.split('=')
+            param_name = param_name_val[0]
+            param_val = param_name_val[1]
+            if param_name not in configuration_results.keys():
+                configuration_results[param_name] = {}
+            if param_val not in configuration_results[param_name].keys():
+                configuration_results[param_name][param_val] = {}
+                configuration_results[param_name][param_val]['test_rho'] = []
+                configuration_results[param_name][param_val]['test_mse'] = []
+                configuration_results[param_name][param_val]['test_pearson_rho'] = []
+                configuration_results[param_name][param_val]['train'] = []
+                configuration_results[param_name][param_val]['train_mse'] = []
+                configuration_results[param_name][param_val]['train_pearson_rho'] = []
+
+            configuration_results[param_name][param_val]['test_rho']+= total_test_rho
+            configuration_results[param_name][param_val]['test_mse']+= total_test_mse
+            configuration_results[param_name][param_val]['test_pearson_rho'] += total_test_pearson_rho
+            configuration_results[param_name][param_val]['train'] += total_train_rho
+            configuration_results[param_name][param_val]['train_mse'] += total_train_mse
+            configuration_results[param_name][param_val]['train_pearson_rho'] += total_train_pearson_rho
+        pass
+    return configuration_results
+
+def plot_per_param(configuration_results):
+    for key, param_values in configuration_results.items():
+        fig, axis = plt.subplots(2, len(list(param_values.values())[0].keys())//2)
+        fig.suptitle(f'{key}', fontsize=16)
+
+        tmp = {}
+
+        for param_value, result_for_param_value in param_values.items():
+            for metric, metric_values in result_for_param_value.items():
+                values_of_param = []
+                for metric_value in metric_values:
+                    if isinstance(metric_value, dict): # rho
+                        value_to_add = metric_value['rho']
+                    else: # mse
+                        value_to_add = metric_value
+                    values_of_param.append(value_to_add)
+                if metric not in tmp.keys():
+                    tmp[metric] = [values_of_param]
+                else:
+                    tmp[metric].append(values_of_param)
+            pass
+        for ax, (key, val) in zip (axis.flatten(), tmp.items()):
+            ax.boxplot(val)
+            ax.set_title(key)
+            ax.set_xticklabels(list(param_values.keys()))
+        pass
+    plt.show()
+    plt.tight_layout()
+    pass
+
+def calc_stats_for_config(grid_search_folder):
     different_configs = [os.path.join(grid_search_folder, o) for o in os.listdir(grid_search_folder)
                          if os.path.isdir(os.path.join(grid_search_folder, o))]
 
@@ -198,9 +322,9 @@ def find_best_config(configuration_stats):
     return best_configs
 
 
-def main():
+def main(grid_search_folder):
     print(f'Analyzing {grid_search_folder}')
-    configuration_stats = calc_stats_for_config()
+    configuration_stats = calc_stats_for_config(grid_search_folder)
     best_configs = find_best_config(configuration_stats)
 
     ## use history... currently not using it...
@@ -208,8 +332,9 @@ def main():
     with open(best_configs['best_test_rho_config']['stats']['hist'], 'rb') as f:
         a = pickle.load(f)
 
-    a=print_recursive(best_configs)
-    pass
+    a=print_recursive2(best_configs)
+    print(a)
+    return a
 
 
 def old_analyze():
@@ -289,4 +414,15 @@ def old_analyze():
 
 
 if __name__ == '__main__':
-    main()
+    grid_search_folder = r'C:\Users\Bar\Desktop\reports\gvhd_multi_grid_tf_with_similiarity'
+
+    # main(grid_search_folder)
+    different_configs = filter_configs(grid_search_folder)
+    configuration_results = calc_result_per_param(different_configs)
+    # plot_per_param(configuration_results)
+
+    different_configs = filter_configs(grid_search_folder, filter_dict={'l2': [1, 20], 'factor': [0.1]})
+    configuration_results = calc_result_per_param(different_configs)
+    plot_per_param(configuration_results)
+
+    plt.show()
