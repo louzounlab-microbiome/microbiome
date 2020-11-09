357ec08729644084c1a9ce7fc683e5e5ecebea93
sharon200102 <sharon200102@gmail.com>
2020-11-09 12:38:41 +0200

    updating the integration and small modifications in otu_and_mapping_file class

--- LearningMethods/create_otu_and_mapping_files.py
+++ LearningMethods/create_otu_and_mapping_files.py
@@ -15,8 +15,9 @@ class CreateOtuAndMappingFiles(object):
         self.otu_path = otu_file_path
         self.tags_path = tags_file_path
         print('read tag file...')
-        self.extra_features_df = pd.read_csv(self.tags_path).drop(['Tag'], axis=1)
-        self.tags_df = pd.read_csv(self.tags_path)[['ID', 'Tag']]
+        mapping_table = pd.read_csv(self.tags_path)
+        self.extra_features_df = mapping_table.drop(['Tag'], axis=1).copy()
+        self.tags_df = mapping_table[['ID', 'Tag']].copy()
         # set index as ID
         self.tags_df = self.tags_df.set_index('ID')
         self.tags_df.index = self.tags_df.index.astype(str)
@@ -25,7 +26,7 @@ class CreateOtuAndMappingFiles(object):
         self.ids = self.tags_df.index.tolist()
         self.ids.append('taxonomy')
         print('read otu file...')
-        self.otu_features_df = pd.read_csv(self.otu_path).drop('Unnamed: 0',axis=1)
+        self.otu_features_df = pd.read_csv(self.otu_path).drop('Unnamed: 0', axis=1)
         self.otu_features_df = self.otu_features_df.set_index('ID')
         self.otu_features_df.index = self.otu_features_df.index.astype(str)
         self.pca_ocj = None
@@ -101,6 +102,55 @@ class CreateOtuAndMappingFiles(object):
             plot_data_2d(X.values, y, data_name=task.capitalize(), save=True, folder=pca_folder)
             if pca >= 3:
                 plot_data_3d(X.values, y, data_name=task.capitalize(), save=True, folder=pca_folder)
+
+    def remove_duplicates(self, keys, filtering_fn=None):
+        """
+        Written by Sharon Komissarov.
+        The function removes duplicates from the mapping table based on the keys inserted i.e, the function will
+        group the mapping table based on the keys list inserted and filter each group using the filtering_fn.
+        finally, the merged and filtered dataframe will be returned. keys: A list of column names that according to
+        them the groupby will be applied.
+        filtering_fn: A groupby function that will filter the groups,
+        default first(), i.e all rows in the group excluding the first will be filtered.
+        """
+        if filtering_fn is None:
+
+            no_duplicates_mapping_table = self.extra_features_df.reset_index().groupby(
+                keys).first().reset_index().set_index('ID')
+        else:
+            no_duplicates_mapping_table = filtering_fn(self.extra_features_df)
+        merged_table = pd.merge(no_duplicates_mapping_table, self.tags_df, left_index=True, right_index=True,
+                                how='left')
+
+        self.extra_features_df = merged_table.drop(['Tag'], axis=1).copy()
+        self.tags_df = merged_table[['Tag']].copy()
+
+    def conditional_identification(self, dic):
+        """
+        Written by Sharon Komissarov.
+        The function facilitate in removing undesired rows by filtering them out.
+        dic: the keys are the names of the columns which according to them the filtering will be applied.
+        the filtering will be applied using the corresponding dic values.
+        for example if you would like to keep only the normal rows, dic should look as follows:
+        dic={'Group':'normal'}
+        if you would like to keep only the normal rows and their saliva samples, dic should look as follows:
+        dic={'Group':'normal','body_site':'saliva'}
+        """
+        mask = pd.DataFrame([self.extra_features_df[key] == val for key, val in dic.items()]).T.all(axis=1)
+        merged_table = pd.merge(self.extra_features_df[mask].copy(), self.tags_df, left_index=True, right_index=True,
+                                how='left')
+        self.extra_features_df = merged_table.drop(['Tag'], axis=1).copy()
+        self.tags_df = merged_table[['Tag']].copy()
+
+    def to_correspond(self, **kwargs):
+        taxonomy=self.otu_features_df.loc['taxonomy'].copy()
+        full_mapping_table = pd.merge(self.extra_features_df, self.tags_df, left_index=True, right_index=True,
+                                      how='left')
+        merged_table = pd.merge(full_mapping_table, self.otu_features_df, **kwargs)
+        self.tags_df = merged_table[['Tag']].copy()
+        self.otu_features_df=merged_table[self.otu_features_df.columns].copy()
+        self.otu_features_df=self.otu_features_df.append(taxonomy)
+        self.extra_features_df=merged_table[self.extra_features_df.columns].copy()
 
 
 if __name__ == "__main__":
--- Projects/GDM/code/main.py
+++ Projects/GDM/code/main.py
@@ -1,13 +1,48 @@
-import  pandas as pd
-from LearningMethods.create_otu_and_mapping_files import CreateOtuAndMappingFiles
-from Preprocess import preprocess_grid
-import os
-bactria_as_feature_file = '../data/data_used/table-with-taxonomy.csv'
-samples_data_file = '../data/data_used/samples_metadata.csv'
-tax = 6
+import pickle
+import torch
+from Projects.GDM.code.data_classes import GDM_data_set
+from torchvision import transforms
+from Projects.GDM.code.transforms_classes import ToTensor
+import matplotlib.pyplot as plt
+from integration_tools.stdvae import StandardVAE
+from integration_tools.mulvae import MultipleVAE
+from integration_tools.utils import make_train_step
+from torch import optim
 
-preprocess_prms = {'taxonomy_level': tax, 'taxnomy_group': 'sub PCA', 'epsilon': 0.1, 'normalization': 'log',
-                       'z_scoring': 'row', 'norm_after_rel': '', 'std_to_delete': 0, 'pca': (0, 'PCA')}
-otuMf = CreateOtuAndMappingFiles(bactria_as_feature_file,samples_data_file)
-otuMf.preprocess(preprocess_prms,visualize=True)
-print(otuMf.otu_features_df)
+with open('../data/data_used/otuMF', 'rb') as otu_file:
+    otuMf = pickle.load(otu_file)
+
+
+GDM_patients_dataset = GDM_data_set(otuMf.otu_features_df, otuMf.extra_features_df, 'womanno.', 'trimester',
+                                    'body_site',
+                                    transform=transforms.Compose([ToTensor()]))
+indexes_of_patients_with_all_fields, indexes_of_patients_with_field1_only, indexes_of_patients_with_field2_only = GDM_patients_dataset.separate_to_groups()
+
+"""
+plt.bar(range(3),[len(indexes_of_patients_with_all_fields),len(indexes_of_patients_with_field1_only),len(indexes_of_patients_with_field2_only)])
+plt.xticks(range(3),labels=['Saliva and stool','Only saliva','Only stool'])
+plt.ylabel('Quantity of patients')
+plt.xlabel('Microbiome types')
+plt.title('GDM Dataset')
+plt.show()
+"""
+
+xy_dataset = torch.utils.data.Subset(GDM_patients_dataset, indexes_of_patients_with_all_fields)
+x_dataset = torch.utils.data.Subset(GDM_patients_dataset, indexes_of_patients_with_field1_only)
+y_dataset = torch.utils.data.Subset(GDM_patients_dataset, indexes_of_patients_with_field2_only)
+xy_dataloader = torch.utils.data.DataLoader(xy_dataset, batch_size=2, shuffle=True, num_workers=0)
+
+sample_size = otuMf.otu_features_df.shape[1]
+latent_layer_size = 10
+lr = 0.001
+activation_fn = torch.nn.Sigmoid()
+xy_vae = StandardVAE([2 * sample_size, sample_size, int(sample_size / 2), latent_layer_size], activation_fn)
+x_vae = StandardVAE([sample_size, int(sample_size / 2), latent_layer_size], activation_fn)
+y_vae = StandardVAE([sample_size, int(sample_size / 2), latent_layer_size], activation_fn)
+full_vae = MultipleVAE(xy_vae, x_vae, y_vae)
+optimizer = optim.Adam(full_vae.parameters(), lr=lr)
+train_step_function = make_train_step(full_vae, optimizer)
+
+for patient_batch in xy_dataloader:
+    field0_batch,field1_batch=patient_batch['FIELD0'],patient_batch['FIELD1']
+    train_step_function(field0_batch,field1_batch)
