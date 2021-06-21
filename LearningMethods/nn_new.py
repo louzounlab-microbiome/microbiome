b7682b9539112fb5ab42d71162679d3d99117bf9
sharon200102 <sharon200102@gmail.com>
2021-06-21 15:34:43 +0300

    modifications in Learning methods

--- LearningMethods/nn_new.py
+++ LearningMethods/nn_new.py
@@ -1,109 +1,171 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from typing import Callable, Any, List,Iterable
+from typing import Callable, Any, List, Iterable
+import pytorch_lightning as pl
+from torch import nn, optim
+from pytorch_lightning.metrics.functional import auc, accuracy, f1
+from sklearn.metrics import roc_auc_score
+
+def _dict_mean(dict_list):
+    mean_dict = {}
+    for key in dict_list[0].keys():
+        mean_dict[key] = sum(d[key] for d in dict_list) / len(dict_list)
+    return mean_dict
+# Global variables which translate the names into the corresponding functions and objects
+activation_fn_dict = {'tanh': nn.Tanh, 'relu': nn.ReLU, 'sigmoid': nn.Sigmoid,
+                      'leaky relu': nn.LeakyReLU}
+optimizers_dict = {'adam': optim.Adam, 'Adadelta': optim.Adadelta, 'Adagrad': optim.Adagrad}
 
 
-class learning_model(nn.Module):
+class BinaryNeuralNetwork(pl.LightningModule):
     """A simple NN which receives as an input an iterable of integers which describes the layers structure except the
     output layer. Additionally, the constructor gets the output size of the model in order to construct the output layer
      and the activation function which will be used in ll internal layers."""
-    def __init__(self, structure_list: Iterable[int], out_size:int, activation_fn=torch.tanh):
-        super(learning_model, self).__init__()
+
+    def __init__(self, input_size: int, internal_list_structure: Iterable[int], activation_fn_name: str = 'relu',
+                 optimizer_name: str = 'adam',
+                 learning_rate: float = 1e-3):
+        super(BinaryNeuralNetwork, self).__init__()
         # construct the layers of the model.
-        self.linears = nn.ModuleList(
-            [nn.Linear(structure_list[i], structure_list[i + 1]) for i in range(0, len(structure_list) - 1)])
-        # create the output layer.
-        self.out = nn.Linear(structure_list[-1], out_size)
-        self.activation_fn = activation_fn
+
+        self.activation_fn = activation_fn_dict[activation_fn_name]()
+        self.optimizer_name = optimizer_name
+        self.learning_rate = learning_rate
+        self.internal_list_structure = internal_list_structure
+        # Insert the first layer
+        modules = self.block(input_size, self.internal_list_structure[0])
+        # Insert the internal layers
+        for i in range(0, len(self.internal_list_structure) - 1):
+            modules.extend(self.block(self.internal_list_structure[i], self.internal_list_structure[i + 1]))
+
+        # Insert the last layer together with the sigmoid function
+        modules.extend(self.block(self.internal_list_structure[-1], 1, activation=False))
+        modules.append(nn.Sigmoid())
+
+        self.modules_list = nn.ModuleList(modules)
+        self.loss_function = nn.BCELoss()
+
+    def block(self, in_feat, out_feat, activation=True):
+        layers = [nn.Linear(in_feat, out_feat)]
+        if activation:
+            layers.append(self.activation_fn)
+        return layers
+
     """The forward function, forwards the input while using the activation function in all layers, except the last 
     one. """
-    def forward(self, input):
-        for layer in self.linears:
-            input = self.activation_fn(layer(input))
-        output = self.out(input)
-        return output
 
-    def predict(self, x, threshold=0.5):
-        """
-        x is the result received after the forward function.
-        the function applies softmax on x and then decides its label according to the result.
-        if threshold is not given, the label is decided based on the dimension with the highest probability.
-        otherwise, the prediction is considered to be binary, and is been made according to the threshold.
-        """
-        # Apply softmax to output.
-        is_binary = self.out.out_features == 2
-        # dim=1 solved me an issue, I think its suits our datasets, but if not check the documentation and alter it.
-        pred = F.softmax(x,dim=1)
-        ans = []
-        # Pick the class with maximum weight
-        for t in pred:
-            if is_binary:
-                if t[1] >= threshold:
-                    ans.append(1)
-                else:
-                    ans.append(0)
-            else:
-                ans.append(t.argmax())
-        return ans
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("BinaryNeuralNetwork")
+        parser.add_argument('--input_size', type=int)
+        parser.add_argument('--internal_list_structure', type=int, nargs='+')
+        parser.add_argument('--activation_fn_name', type=str, default='relu')
+        parser.add_argument('--optimizer_name', type=str, default='adam')
+        parser.add_argument('--learning_rate', type=float, default=1e-3)
+        return parent_parser
 
-    def predict_prob(self, input):
-        """A function that runs the input through the net and provides the probabilities of each class."""
-        # dim=1 solved me an issue, I think its suits our datasets, but if not check the documentation and alter it.
+    def forward(self, x):
+        for module in self.modules_list:
+            x = module(x)
+        return x
 
-        return F.softmax(self(input),dim=1)
+    def configure_optimizers(self):
+        return optimizers_dict[self.optimizer_name](self.parameters(), lr=self.learning_rate)
 
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = torch.squeeze(self(x))
+        if len(logits.shape) == 0:
+            logits = torch.unsqueeze(logits,dim=0)
+        loss = self.loss_function(logits, y.type(torch.float))
+        acc = accuracy(logits, y)
+        self.log('train_accuracy',acc,prog_bar=True,on_step=False,on_epoch=True)
+        return {'loss': loss, 'train_acc': acc}
 
-def make_train_step(model, loss_fn, optimizer):
-    # Builds function that performs a step in the train loop
-    def train_step(x, y):
-        # Sets model to TRAIN mode
-        model.train()
-        # Makes predictions
-        yhat = model(x)
-        # Computes loss
-        loss = loss_fn(yhat, y)
-        # Computes gradients
-        loss.backward()
-        # Updates parameters and zeroes gradients
-        optimizer.step()
-        optimizer.zero_grad()
-        # Returns the loss
-        return loss.item()
+    def validation_step(self, batch, batch_idx):
+        results = self.training_step(batch, batch_idx)
+        results['validation_acc'] = results['train_acc']
+        del results['train_acc']
+        return results
 
-    # Returns the function that will be called inside the train loop
-    return train_step
+    def validation_epoch_end(self, outputs: List[Any]) -> None:
+        avg_val_loss = torch.tensor([output['loss'] for output in outputs]).mean()
+        avg_val_acc = torch.tensor([output['validation_acc'] for output in outputs]).mean()
 
+        self.log('val_loss', avg_val_loss, prog_bar=True)
+        self.log('validation_acc', avg_val_acc, prog_bar=True)
 
-def get_weights_out_of_target(target_series, weights_fn=None):
-    if weights_fn is None:
-        weights_fn = lambda input: 1 / input
-    target_unique_elements = sorted(list(target_series.unique()))
-    quantity_of_target_unique_elements = [list(target_series).count(i) for i in target_unique_elements]
-    normedWeights = list(map(weights_fn, quantity_of_target_unique_elements))
-    normedWeights = torch.FloatTensor(normedWeights)
-    return normedWeights
+    def test_step(self, batch, batch_idx):
+        # please notice that the test step is assuming that the whole test is passed as a unit.
+        # Use LearningDataSet located in the simple_dataset module in order to achieve that.
+
+        x, y = batch
+        logits = torch.squeeze(self(x))
+        acc = accuracy(logits, y)
+        f1_score = f1(logits, y, 1)
+        auc_score = roc_auc_score(y, logits)
+        metrics_dict = {'acc': acc.item(), 'f1_score': f1_score.item(), 'auc_score': auc_score}
+        self.test_predictions = logits
+        self.log_dict(metrics_dict)
 
 
-def calculate_f1_score(precision, recall):
-    return 2 * (precision * recall) / (precision + recall)
+if __name__ == '__main__':
+    # The purpose of this script is to allow multiple runs of the new_nn module on different datasets
+    # and to compare the results.
+    # Specific import for the script, not for the module.
+    from argparse import ArgumentParser
+    from subprocess import check_output
+    import yaml
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import matplotlib
 
+    matplotlib.style.use('ggplot')
+    parser = ArgumentParser()
+    # add PROGRAM level args
+    # A file which includes the command line arguments needed for the nn_classification script for all different trail.
+    # Each in a different line
+    # Check microbiome/Projects/GDM/experiment1/code/learning_code/config.txt for instance.
+    parser.add_argument('--arguments_file', '-a', type=str)
+    parser.add_argument('--cwd', '-c', type=str,default='.')
+    # A path to a directory that the nn_classification script will be executed from.
+    parser.add_argument('--experiments_names', '-n', type=str, nargs='+')
+    parser.add_argument('--title','-t',type=str,default='Performance comparison')
+    parser.add_argument('--cross_validation','-cv', default= 1,type=int)
+    # The different trail names.
+    args = parser.parse_args()
 
-def best_threshold(precision_arr, recall_arr, thresholds_arr, index_function=None, maximize=True):
-    if index_function is None:
-        index_function = calculate_f1_score
-    index_values = [index_function(precision, recall) for precision, recall in zip(precision_arr, recall_arr)]
-    if maximize:
-        best_value = max(index_values)
-    else:
-        best_value = min(index_values)
-    return thresholds_arr[index_values.index(best_value)], best_value
+    test_dict_list = []
+    arguments_file = open(args.arguments_file, 'r')
+    command_line_argument_list = arguments_file.readlines()
+    # Read the commands line by line
+    for command_line_arguments in command_line_argument_list:
+        if command_line_arguments == '\n':
+            break
+        else:
+            cross_validation_test_dict_list = []
+            for k in range(args.cross_validation):
+                # execute a trail using the command line arguments inserted.
+                output = check_output(['python3', 'nn_classification_runner.py','--seed',str(k)] + command_line_arguments.split(' '), stdin=None,
+                                      cwd=args.cwd).decode("utf-8")
 
+                # The next two lines are an ugly way to extract the test results out of the output
+                output = output.split('--------------------------------------------------------------------------------')[1]
+                test_str = output.split('TEST RESULTS')[1]
+                test_dict = yaml.load(test_str)
+                cross_validation_test_dict_list.append(test_dict)
+            final_test_dict = _dict_mean(cross_validation_test_dict_list)
+            test_dict_list.append(final_test_dict)
 
-def early_stopping(history, patience=2, ascending=True):
-    if len(history) <= patience:
-        return False
-    if ascending:
-        return history[-patience - 1] == max(history[-patience - 1:])
-    else:
-        return history[-patience - 1] == min(history[-patience - 1:])
+    # Plot the trails results
+    exp_result_df = pd.DataFrame(test_dict_list, index=args.experiments_names).transpose()
+    ax = exp_result_df.plot.bar(rot=0)
+    ax.set_title(args.title)
+    # add annotation
+    for p in ax.patches:
+        ax.annotate(str(round(p.get_height(),2)), (p.get_x() * 1.005, p.get_height() * 1.005))
+
+    plt.show()
+
+# --arguments_file /home/sharon200102/Documents/second_degree/Research/microbiome_projects/microbiome/Projects/GDM/experiment1/code/learning_code/config.txt -n GAN_results Saliva_results Stool_results
